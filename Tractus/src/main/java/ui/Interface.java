0983a5ac86bc898a87c789c96c5fb6fca2867d91
Juho Juutilainen <juho.juutilainen@meteoriitti.com>
2019-06-06 21:06:22 +0300

    ui piirtämistä muutettu

--- Tractus/src/main/java/ui/Interface.java
+++ Tractus/src/main/java/ui/Interface.java
@@ -7,6 +7,7 @@ import domain.Creature;
 import domain.Command;
 import domain.World;
 import engine.MonsterController;
+import helpers.Distance;
 
 import javax.swing.*;
 import java.awt.*;
@@ -28,10 +29,14 @@ public class Interface extends JFrame {
     private MonsterController monsterController;
     private PlayerController playerController;
     private World world;
+    private Distance distance;
+    private int centerX;
+    private int centerY;
+    private int viewDistance;
 
     /**
-     * Constructor that receives parameters and objects used for output
-     * and receiving player actions.
+     * Constructor that receives parameters and objects used for output and
+     * receiving player actions.
      *
      * @param world contains and controls the map
      * @param playerController receives imput from ui and controls player-object
@@ -52,12 +57,16 @@ public class Interface extends JFrame {
         this.playerController = playerController;
 
         this.terminal = new AsciiPanel(this.viewportWidth, this.viewportHeight, AsciiFont.DRAKE_10x10);
-
+        this.distance = new Distance();
         super.add(terminal);
         super.addKeyListener(keylistener);
         super.setSize(this.viewportWidth * 10, this.viewportHeight * 10);
         super.setVisible(true);
         super.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        
+        this.centerX = this.viewportWidth / 2;
+        this.centerY = this.viewportWidth / 2;
+        this.viewDistance = Math.min(this.viewportWidth, this.viewportWidth) / 2 - 2;
     }
 
     /**
@@ -73,16 +82,21 @@ public class Interface extends JFrame {
     }
 
     private void drawMap(int startX, int startY) {
+
         for (int x = 0; x < viewportWidth; x++) {
             for (int y = 0; y < viewportHeight; y++) {
-                char c = (char) 177;
-                if (this.world.getTerrain(x + startX, y + startY) == 1) {
-                    c = (char) 250;
+                if (this.distance.getDistance(this.centerX, this.centerY, x, y) < this.viewDistance) {
+                    char c = (char) 177;
+                    if (this.world.getTerrain(x + startX, y + startY) == 1) {
+                        c = (char) 250;
+                    }
+                    this.terminal.write(c, x, y);
                 }
-                this.terminal.write(c, x, y);
             }
         }
     }
+
+    
 
     private void drawCreatures(int startX, int startY) {
         this.terminal.write("@", this.playerController.getPlayerPosition()[0] - startX, this.playerController.getPlayerPosition()[1] - startY);
@@ -91,29 +105,29 @@ public class Interface extends JFrame {
         for (int i = 0; i < monsterPositions.length; i++) {
             int monsterX = monsterPositions[i][0] - startX;
             int monsterY = monsterPositions[i][1] - startY;
-            if (monsterX > 0 && monsterX < viewportWidth && monsterY > 0 && monsterY < viewportHeight) {
+            
+            if (this.distance.getDistance(this.centerX, this.centerY, monsterX, monsterY) < this.viewDistance) {
                 this.terminal.write("*", monsterX, monsterY);
             }
         }
     }
-    
+
     /**
      * Draw's a single character on AsciiPanel
-     * 
+     *
      * @param character character to be drawn
      * @param x x-coordinate of the character
      * @param y y-coordinate of the character
      */
-    
     public void drawCharacter(char character, int x, int y) {
-        int originX =  this.viewportWidth / 2 - this.playerController.getPlayerPosition()[0];
-        int originY =  this.viewportHeight / 2 - this.playerController.getPlayerPosition()[1];
-        int drawX = x+originX;
-        int drawY = y+originY;
+        int originX = this.viewportWidth / 2 - this.playerController.getPlayerPosition()[0];
+        int originY = this.viewportHeight / 2 - this.playerController.getPlayerPosition()[1];
+        int drawX = x + originX;
+        int drawY = y + originY;
         if (drawX > 0 && drawX < this.viewportWidth && drawY > 0 && drawY < this.viewportHeight) {
-            this.terminal.write(character,drawX,drawY);
+            this.terminal.write(character, drawX, drawY);
         }
-        
+
     }
 
     /**
@@ -122,9 +136,10 @@ public class Interface extends JFrame {
     public void clear() {
         terminal.clear();
     }
-    
+
     /**
      * Shows a short message to player.
+     *
      * @param message message content
      */
     public void message(String message) {
